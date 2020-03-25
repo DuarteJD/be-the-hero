6c6b44a8ca8048163faeec554ada9cabda2b3669
Julio Duarte <jcduarte.ti@gmail.com>
2020-03-24 23:47:57 -0300

    Criação do backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+/** 
+ * O express, antes de chemar a rota irá converter o corpo da requisição em um objeto JSON.
+ * É importante que esta codificação venha antes das rotas.
+ */
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Tipos de parâmetros:
+ * Query Params: Parametros nomeados enviados na rota, após o "?"" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos, após o "/"
+ * Request Body: Corpo da requisição, utilizado para criar/alterar um recurso.
+ * 
+ * request.query;
+ * request.params;
+ * request.body;
+ */
+
+ /**
+  * Utilizar um QueryBuilder não nos preocupamos com o banco de dados
+  * que a aplicação irá trabalhar, já que iremos escrever as query's
+  * utilizando um syntaxe JS.
+  * 
+  * http://knexjs.org/
+  */
+
+app.listen(3333);
